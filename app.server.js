677e30fa44f4040343a2938af219c21dce4b4549
StevenJBurns <stevenjburns@msn.com>
2018-05-24 16:19:13 -0600

    Added css, images and js to public folder; Added app.get statement test -- server non-responsive

--- app.server.js
+++ app.server.js
@@ -11,7 +11,11 @@ require("dotenv").config();
 
 let app = new express();
 
+app.get("*", (req, res) => console.log(chalk.bgBlue.white(` Incoming GET request -- URL: ${req.url} `)));
 
+app.get("/", (req, res) => res.send("Welcome Home"));
+
+app.get("/admin", (req, res) => res.send("Welcome to Admin"));
 
 app.listen(process.env.PORT, () => {
   console.clear();
