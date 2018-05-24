f83f7ee1eef13380105a08d80da4bc27cb48e73f
StevenJBurns <stevenjburns@msn.com>
2018-05-24 15:20:12 -0600

    Chalk and Express dependcies added; app created with listening callback; works good

--- app.server.js
+++ app.server.js
@@ -4,4 +4,16 @@ const fs = require("fs");
 const url = require("url");
 const path = require("path");
 
+const chalk = require("chalk");
+const express = require("express");
+
 require("dotenv").config();
+
+let app = new express();
+
+
+
+app.listen(process.env.PORT, () => {
+  console.clear();
+  console.log(chalk.green(`Express Server start.  Listening on port ${process.env.PORT}`));
+});
