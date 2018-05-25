0bd920d8439c68208f0171362c74205060d1c187
StevenJBurns <stevenjburns@msn.com>
2018-05-25 14:03:52 -0600

    app.server.js syntax and formatting clean up

--- app.server.js
+++ app.server.js
@@ -1,22 +1,26 @@
 /* dependencies */
-
 const fs = require("fs");
 const url = require("url");
 const path = require("path");
-
 const chalk = require("chalk");
 const express = require("express");
 
+/* connect Node process.env to .env file */
 require("dotenv").config();
 
+/* create server similar to Node http */
 let app = new express();
 
-app.get("*", (req, res) => console.log(chalk.bgBlue.white(` Incoming GET request -- URL: ${req.url} `)));
+/* set up some basic routes */
+app.get("*", (req, res, next) => {
+   console.log(chalk.bgBlue.white(` Incoming GET request -- URL: ${req.url} `));
+   next();
+});
 
-app.get("/", (req, res) => res.send("Welcome Home"));
+app.get("/", (req, res) => res.end("Welcome Home"));
+app.get("/admin", (req, res) => res.end("Welcome to Admin"));
 
-app.get("/admin", (req, res) => res.send("Welcome to Admin"));
-
+/* attach listener to server */
 app.listen(process.env.PORT, () => {
   console.clear();
   console.log(chalk.bgBlackBright.white(` ** Express Server started.  Listening on port ${process.env.PORT} ** `));
