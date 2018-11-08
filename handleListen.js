ac167cf4a9e1855507f25678e77827907e93c889
StevenJBurns <stevenjburns@msn.com>
2018-11-08 16:57:49 -0700

    Bumped version to 0.2.0; added Express router module; installed serve-favicon via NPM; general cleanup across modules and package.json

--- handleListen.js
+++ handleListen.js
@@ -1,8 +1,6 @@
 const chalk = require("chalk");
-const dotenv = require("dotenv");
 
 module.exports = handleListen = (port) => {
-    //console.clear();
     console.log(chalk.bgBlackBright.white(` Express Server started: ${process.env.APPNAME} `));
     console.log(chalk.bgBlackBright.white(` Listening on port ${port} `));
 }
--- handleRequest.js
+++ handleRequest.js
@@ -2,9 +2,6 @@ const chalk = require("chalk");
 
 module.exports = handleRequest = (req, res, next) => {
   console.log(chalk.bgBlue.white(` Incoming ${req.method} request -- URL: ${req.url} `));
-
-  // (req, res) => res.end("Welcome Home")
-  // (req, res) => res.end("Welcome to Admin")
-
+  
   next();
 }
